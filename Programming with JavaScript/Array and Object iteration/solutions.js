cbaab07f58e41597e5deb87ebfec261790732c31
Dhruv Mehta <60223932+Dante-afk@users.noreply.github.com>
2022-10-30 21:39:31 +0530

    Create solutions.js

---
+++ Programming with JavaScript/Array and Object iteration/solutions.js
@@ -0,0 +1,43 @@
+// Task 1
+var dairy = ['cheese', 'sour cream', 'milk', 'yogurt', 'ice cream', 'milkshake']
+
+function logDairy() {
+    for(var items of dairy){
+        console.log(items)
+    }
+}
+
+// Task 2
+const animal = {
+
+    canJump: true
+
+};
+
+const bird = Object.create(animal);
+
+bird.canFly = true;
+
+bird.hasFeathers = true;
+
+function birdCan() {
+    for (var prop of Object.keys(bird)) {
+        console.log(prop + ": " +  bird[prop])
+    }
+}
+
+// Task 3
+function animalCan() {
+    for (var prop of Object.keys(bird)) {
+        console.log(prop + ": " +  bird[prop])
+    }
+    for (var prop of Object.keys(animal)) {
+        console.log(prop + ": " +  animal[prop])
+    }
+}
+
+
+logDairy();
+birdCan();
+animalCan();
+
